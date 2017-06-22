e611eb8b614dba8aa42cf5a23eeb0ab973fd0cd7
haknudsen <paris_giovanni@yahoo.com>
2017-06-22 14:42:01 -0600

    updated footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,23 +1,27 @@
-<div class="footer"> © <?php echo date("Y")?>, <a href="http://websitetalkingheads.com" target="_blank">WebsiteTalkingHeads.com</a>. All Rights Reserved. </div>
+<footer>
+    <div class="footer">
+        ©
+        <?php echo date("Y")?>, <a href="http://websitetalkingheads.com" target="_blank">TalkingHeads.com</a>. All Rights Reserved.
+        <h2 class="footer-title"><a href="http://talkingheads.com/affiliate/">Powered by <span class="blue">Talking</span> Heads<sup class="tm-small">&#174;</sup></a></h2>
+    </div>
+</footer>
 <!-- Google Code for Previous Visitors --> 
 <!-- Remarketing tags may not be associated with personally identifiable 
-information or placed on pages related to sensitive categories. For 
-instructions on adding this tag and more information on the above 
-requirements, read the setup guide: google.com/ads/remarketingsetup --> 
-<script type="text/javascript">
-/* <![CDATA[ */
-var google_conversion_id = 1058129782;
-var google_conversion_label = "-jOyCO6ZkAIQ9o7H-AM";
-var google_custom_params = window.google_tag_params;
-var google_remarketing_only = true;
-/* ]]> */
-</script> 
-<script type="text/javascript" 
-src="//www.googleadservices.com/pagead/conversion.js">
+information or placed on pages related to sensitive categories . For
+instructions on adding this tag and more information on the above
+requirements, read the setup guide: google . com / ads / remarketingsetup -->
+    <script type="text/javascript">
+        /* <![CDATA[ */
+        var google_conversion_id = 1058129782;
+        var google_conversion_label = "-jOyCO6ZkAIQ9o7H-AM";
+        var google_custom_params = window.google_tag_params;
+        var google_remarketing_only = true;
+        /* ]]> */
+    </script>
+<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
 </script>
 <noscript>
-<div style="display:inline;"> <img height="1" width="1" style="border-style:none;" alt="" 
-src="//googleads.g.doubleclick.net/pagead/viewthroughconversion/1058129782/?value=0&amp;label=-jOyCO6ZkAIQ9o7H-AM&amp;guid=ON&amp;script=0"/> </div>
+    <div style="display:inline;"> <img height="1" width="1" style="border-style:none;" alt="" src="//googleads.g.doubleclick.net/pagead/viewthroughconversion/1058129782/?value=0&amp;label=-jOyCO6ZkAIQ9o7H-AM&amp;guid=ON&amp;script=0"/> </div>
 </noscript>
 
 <script type="text/javascript" src="http://websitetalkingheads.com/forms/js/validate.js"></script>
