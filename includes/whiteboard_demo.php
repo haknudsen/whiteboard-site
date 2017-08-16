0cddc05356efc30202cfa522324dfe78614ccaf2
Andy Knudsen <andy@websitetalkingheads.com>
2017-08-16 03:38:13 -0600

    Made the top 9 list a list with the place of the viideo

--- includes/whiteboard_demo.php
+++ includes/whiteboard_demo.php
@@ -16,6 +16,7 @@ switch ( $columns ) {
     default:
         $span = 4;
 }
+$alt = array("Whiteboard","Whiteboard Video","Whiteboard Animation","Explainer Videos","Animated Explainer","Drawing Animation","Doodle Video","Sketch Video","Chalkboard Video","Custom Animation","Explainer Video","Website Video","Doodle Animation","Animated Video","Drawn Typography","Whiteboard Animation","Web Video","Video Production","Custom Video");
 $link = '" href="https://www.youtube.com/watch?v=';
 $class = '&rel=0&autoplay=1&hd=1" class="lightbox">';
 if ( $result->num_rows > 0 ) {
@@ -25,11 +26,13 @@ if ( $result->num_rows > 0 ) {
     while ( $row = $result->fetch_assoc() ) {
         $target = $row[ "target" ];
         $video = $row[ "name" ];
+        $type = array_rand($alt, 1);
+        $id = $video . " - " . $alt[$type];
         echo '<div class="col-sm-' . $span . '">';
         echo PHP_EOL;
-        echo '<a title="' . $video . $link . $target .  $class . '<img src="https://img.youtube.com/vi/' . $target . '/maxresdefault.jpg" class="img img-responsive box" id="' . $video . '" alt="Custom Video Presentation Example - ' . $example->name . ' " ></a>';
+        echo '<a title="' . $video . $link . $target .  $class . '<img src="https://img.youtube.com/vi/' . $target . '/maxresdefault.jpg" class="img img-responsive box" id="' . $id . '" alt="' . $id . ' Example' . $example->name . ' " ></a>';
         echo PHP_EOL;
-        echo '<h3 class="example-text"><a title="' . $video . $link . $target . $class . $video . '</a></h3>';
+        echo '<h3 class="example-text"><a title="' . $id . $link . $target . $class . '#' . $x . " " . $video . '</a></h3>';
         echo PHP_EOL;
         echo '</div>';
         echo PHP_EOL;
