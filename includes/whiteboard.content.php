8f5a58ce97c8c6c9c1460e94002fbb7cd5b09d46
Andy Knudsen <andy@websitetalkingheads.com>
2017-10-19 19:29:20 -0600

    whitebaord

---
+++ includes/whiteboard.content.php
@@ -0,0 +1,41 @@
+<?php
+require( "connect.php" );
+$sql = "SELECT * FROM whiteboard_content ORDER BY RAND() LIMIT 1";
+$result = $conn->query( $sql );
+if ( $result->num_rows > 0 ) {
+    echo PHP_EOL;
+    while ( $row = $result->fetch_assoc() ) {
+        $name = $row[ "title" ];
+        $content = $row[ "content"];
+        $spun = $spintax->process( $content );
+        echo( '<div class="container">' );
+        echo PHP_EOL;
+        echo( '<h2 class="text-center">' .$title . '</h2>');
+        echo PHP_EOL;
+        echo('<div class="lead">');
+        echo PHP_EOL;
+        echo( $spun);
+        echo PHP_EOL;
+        echo( '   </div>' );
+        echo PHP_EOL;
+    }
+} else {
+    echo "0 results";
+}
+echo PHP_EOL;
+class Spintax {
+    public
+    function process( $text ) {
+        return preg_replace_callback( '/\{(((?>[^\{\}]+)|(?R))*)\}/x', array(
+            $this,
+            'replace'
+        ), $text );
+    }
+    public
+    function replace( $text ) {
+        $text = $this->process( $text[ 1 ] );
+        $parts = explode( '|', $text );
+        return $parts[ array_rand( $parts ) ];
+    }
+}
+?>
